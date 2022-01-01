1e4451a7eb7e756a99b32d6d3356ae40551fd6f8
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2021-12-31 21:45:34 -0800

    1231

---
+++ binaryTree/98-Validate Binary Search Tree.py
@@ -0,0 +1,30 @@
+# Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+# A valid BST is defined as follows:
+
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+
+
+
+# set a bound, left < target.val < right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root):
+        return self.valid(root, -sys.maxsize, sys.maxsize)
+    
+    def valid(self, root, left, right):
+        if root == None:
+            return True
+        if(root.val <= left):
+            return False
+        if(root.val >= right):
+            return False
+        
+        return self.valid(root.left, left, root.val) and self.valid(root.right, root.val, right)
