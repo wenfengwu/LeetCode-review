38630f1912a23c8decb57243513ef37793aabd77
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-01 20:19:24 -0800

    0101

---
+++ string/38-Count and Say.py
@@ -0,0 +1,44 @@
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+# To determine how you "say" a digit string, split it into the minimal number of groups so that each group is a contiguous section all of the same character. Then for each group, say the number of characters, then say the character. To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+
+# Example 1:
+
+# Input: n = 1
+# Output: "1"
+# Explanation: This is the base case.
+# Example 2:
+
+# Input: n = 4
+# Output: "1211"
+# Explanation:
+# countAndSay(1) = "1"
+# countAndSay(2) = say "1" = one 1 = "11"
+# countAndSay(3) = say "11" = two 1's = "21"
+# countAndSay(4) = say "21" = one 2 + one 1 = "12" + "11" = "1211"
+ 
+
+# Constraints:
+
+# 1 <= n <= 30
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        val = ""
+        res = "1"
+        
+        for _ in range(n-1):
+            cnt = 1
+            for j in range(len(res)-1):
+                # if they are equal
+                if res[j]==res[j+1]:
+                    cnt+=1
+                else:
+                    val += str(cnt) + res[j]
+                    cnt = 1
+            val += str(cnt)+res[-1]
+            res = val
+            val = ""
+        return res
