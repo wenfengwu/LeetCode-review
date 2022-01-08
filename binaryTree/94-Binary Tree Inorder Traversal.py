a8ea45f9b00fe35d9606c0129548f2f95ed7134b
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-07 18:28:49 -0800

    tree is sosososo hard!!!

---
+++ binaryTree/94-Binary Tree Inorder Traversal.py
@@ -0,0 +1,32 @@
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# Example 2:
+
+# Input: root = []
+# Output: []
+# Example 3:
+
+# Input: root = [1]
+# Output: [1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root == None:
+            return []
+        result = []
+        self.helper(root, result)
+        return result
+    
+    def helper(self, root, res):
+        if root:
+            self.helper(root.left, res)
+            res.append(root.val)
+            self.helper(root.right, res)
