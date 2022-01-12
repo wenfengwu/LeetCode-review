0dfa5d5cd97a190688d655b2f6f5140e7d41c54c
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-11 21:24:23 -0800

    0111

---
+++ dp/62-Unique Paths.py
@@ -0,0 +1,21 @@
+# There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+# Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+# Input: m = 3, n = 2
+# Output: 3
+# Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# 1. Right -> Down -> Down
+# 2. Down -> Down -> Right
+# 3. Down -> Right -> Down
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for x in range(n)] for x in range(m)]
+        for i in range(m): #row
+            for j in range(n): #column
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
