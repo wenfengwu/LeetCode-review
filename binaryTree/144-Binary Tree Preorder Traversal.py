c2a53fd94814fca6aaab56b70bdbccb318be663d
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-12 21:13:10 -0800

    0112

---
+++ binaryTree/144-Binary Tree Preorder Traversal.py
@@ -0,0 +1,35 @@
+# Input: root = [1,null,2,3]
+# Output: [1,2,3]
+# Example 2:
+
+# Input: root = []
+# Output: []
+# Example 3:
+
+# Input: root = [1]
+# Output: [1]
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.preOrder(root, result)
+        return result
+    
+    def preOrder(self, root, result):
+        if root == None:
+            return
+        result.append(root.val)
+        self.preOrder(root.left, result)
+        self.preOrder(root.right, result)
