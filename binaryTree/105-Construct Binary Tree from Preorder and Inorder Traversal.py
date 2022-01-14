5700825456c3a934eb38d884eb6a47099c46cbed
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-13 18:48:17 -0800

    0113

---
+++ binaryTree/105-Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -0,0 +1,56 @@
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+# Example 2:
+
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+ 
+
+# Constraints:
+
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# solution 01
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        return self.helper(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1)
+    
+    def helper(self, preorder, preStart, preEnd, inorder, inStart, inEnd):
+        if(preStart > preEnd):
+            return
+        
+        rootVal = preorder[preStart]
+        root = TreeNode(rootVal)
+        idx = inorder.index(rootVal)
+        
+        leftSize = idx - inStart
+        root.left = self.helper(preorder, preStart + 1, preStart + leftSize,
+                      inorder, inStart, idx - 1)
+        root.right = self.helper(preorder, preStart + leftSize + 1, preEnd,
+                       inorder, idx + 1, inEnd)
+        
+        return root
+
+# solution 02
+
+def buildTree(self, preorder, inorder):
+if inorder:
+    ind = inorder.index(preorder.pop(0))
+    root = TreeNode(inorder[ind])
+    root.left = self.buildTree(preorder, inorder[0:ind])
+    root.right = self.buildTree(preorder, inorder[ind+1:])
+    return root
