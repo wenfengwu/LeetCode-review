c10aea51f59afd9c8ea14d9bad31c0dd954cb969
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-13 18:48:26 -0800

    0113

---
+++ binaryTree/106-Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,38 @@
+# Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
+
+# Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+# Output: [3,9,20,null,null,15,7]
+# Example 2:
+
+# Input: inorder = [-1], postorder = [-1]
+# Output: [-1]
+ 
+
+# Constraints:
+
+# 1 <= inorder.length <= 3000
+# postorder.length == inorder.length
+# -3000 <= inorder[i], postorder[i] <= 3000
+# inorder and postorder consist of unique values.
+# Each value of postorder also appears in inorder.
+# inorder is guaranteed to be the inorder traversal of the tree.
+# postorder is guaranteed to be the postorder traversal of the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
+        
+        root = TreeNode(postorder.pop())
+        inorderIndex = inorder.index(root.val)
+
+        root.right = self.buildTree(inorder[inorderIndex+1:], postorder)
+        root.left = self.buildTree(inorder[:inorderIndex], postorder)
+
+        return root
