6138c810de6188baf82e8567ba03206ee9672d6c
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-16 20:55:48 -0800

    0116

---
+++ binaryTree/450-Delete Node in a BST.py
@@ -0,0 +1,42 @@
+# Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+# Basically, the deletion can be divided into two stages:
+
+# Search for a node to remove.
+# If the node is found, delete the node.
+
+# Input: root = [5,3,6,2,4,null,7], key = 3
+# Output: [5,4,6,2,null,null,7]
+# Explanation: Given key to delete is 3. So we find the node with value 3 and delete it.
+# One valid answer is [5,4,6,2,null,null,7], shown in the above BST.
+# Please notice that another valid answer is [5,2,6,null,4,null,7] and it's also accepted.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        if not root: return None
+        if root.val == key:
+            if root.left:
+                # Find the right most leaf of the left sub-tree
+                left_right_most = root.left
+                while left_right_most.right:
+                    left_right_most = left_right_most.right
+                # Attach right child to the right of that leaf
+                left_right_most.right = root.right
+                # Return left child instead of root, a.k.a delete root
+                return root.left
+            else:
+                return root.right
+            # If left or right child got deleted, the returned root is the child of the deleted node.
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
+
+        return root
