1d0c087df9bef8c5d0795fcfc3d8e9eb47d64515
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-20 17:52:32 -0800

    0120

---
+++ binaryTree/112-Path Sum.py
@@ -0,0 +1,44 @@
+# Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+# A leaf is a node with no children.
+
+# Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+# Output: true
+# Explanation: The root-to-leaf path with the target sum is shown.
+
+# Input: root = [1,2,3], targetSum = 5
+# Output: false
+# Explanation: There two root-to-leaf paths in the tree:
+# (1 --> 2): The sum is 3.
+# (1 --> 3): The sum is 4.
+# There is no root-to-leaf path with sum = 5.
+
+# Input: root = [], targetSum = 0
+# Output: false
+# Explanation: Since the tree is empty, there are no root-to-leaf paths.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return
+        res=[]
+        self.dfs(root,targetSum, res)
+        #The any() function returns True if any element of an iterable is True. If not, it returns False
+        return any(res)
+    
+    def dfs(self, root, targetSum, res):
+        if not root:
+            return
+        if root.val == targetSum and not root.left and not root.right:
+            return res.append(True)
+        if root.left:
+            self.dfs(root.left, targetSum-root.val, res)
+        if root.right:
+            self.dfs(root.right, targetSum-root.val, res)
+        
