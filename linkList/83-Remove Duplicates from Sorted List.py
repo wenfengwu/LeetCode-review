58925f66abbfa0e36ae58c82ed81ad1e16ac9d3d
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-28 15:30:21 -0800

    0128

---
+++ linkList/83-Remove Duplicates from Sorted List.py
@@ -0,0 +1,21 @@
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+# Input: head = [1,1,2]
+# Output: [1,2]
+
+# Input: head = [1,1,2,3,3]
+# Output: [1,2,3]
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = head
+        while cur:
+            while cur.next and cur.val == cur.next.val:
+                cur.next = cur.next.next
+            cur = cur.next
+        return head
