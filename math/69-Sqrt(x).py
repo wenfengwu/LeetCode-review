2fc81243a58dc3851912bec4eed6da9b1b285586
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-28 15:30:30 -0800

    0128

---
+++ math/69-Sqrt(x).py
@@ -0,0 +1,36 @@
+# Given a non-negative integer x, compute and return the square root of x.
+
+# Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+# Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+# Example 1:
+
+# Input: x = 4
+# Output: 2
+# Example 2:
+
+# Input: x = 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
+ 
+
+# Constraints:
+
+# 0 <= x <= 231 - 1
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > x:
+                right = mid - 1
+            elif mid*mid < x:
+                left = mid + 1
+            else:
+                return mid
+        
+        return right
