a8a0a26226021ee116e3a019e151f0937306c430
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-01-29 21:16:00 -0800

    0129

--- dp/300-Longest Increasing Subsequence.py
+++ dp/300-Longest Increasing Subsequence.py
@@ -59,3 +59,13 @@ class Solution:
                 dp[i] = x
         print(dp)
         return len(dp)
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
