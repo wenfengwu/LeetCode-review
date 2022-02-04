328154bb6687822457748a4923119194824a16bf
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-02-03 19:26:50 -0800

    0203

--- array/977-Squares of a Sorted Array.js
+++ array/977-Squares of a Sorted Array.js
@@ -39,3 +39,18 @@ var sortedSquares = function(nums) {
     }
     return tempArr.reverse();
 };
+
+//poyhon solution   
+// class Solution:
+//     def sortedSquares(self, nums: List[int]) -> List[int]:
+//         result=[]
+//         left = 0
+//         right = len(nums) - 1
+//         while left <= right:
+//             if abs(nums[left]) < abs(nums[right]):
+//                 result.append(nums[right]**2)
+//                 right -= 1
+//             else:
+//                 result.append(nums[left]**2)
+//                 left += 1
+//         return result[::-1]
