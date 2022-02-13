c02a0f15ccf927d69841ee9265539be1dfbf392d
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2022-02-12 22:13:41 -0800

    0212

--- linkList/160-Intersection of Two Linked Lists.py
+++ linkList/160-Intersection of Two Linked Lists.py
@@ -10,11 +10,14 @@ class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         p1 = headA
         p2 = headB
+        #while two pointers has not intersection
         while p1 != p2:
+            #if p1 reach the end, move it to headB
             if p1 == None:
                 p1 = headB
             else:
                 p1 = p1.next
+            #if p2 reach the end, move it to headA
             if p2 == None:
                 p2 = headA
             else:
